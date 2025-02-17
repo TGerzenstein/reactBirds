d3bcd48f8ee58271a0731dd98f55773148402bc2
Tatiana <tatigerzens@gmail.com>
2025-02-17 17:14:18 -0300

    added information for blog section

--- appBirds/src/App.tsx
+++ appBirds/src/App.tsx
@@ -1,13 +1,17 @@
 import './App.css'
 //import HomePage from '../src/pages/HomePage.jsx';
-import AboutUsPage from '../src/pages/AboutUsPage.jsx';
+//import AboutUsPage from '../src/pages/AboutUsPage.jsx';
+import BlogPage from '../src/pages/BlogPage.jsx';
+
 
 function App() {
 
   return (
     <>
-{/*         <HomePage></HomePage>
- */}        <AboutUsPage></AboutUsPage>
+{/*   <HomePage></HomePage>
+      <AboutUsPage></AboutUsPage>*/}  
+      <BlogPage></BlogPage>
+
      </>
   )
 }
